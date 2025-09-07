0618c8346571f50784b102485b78015f0b61840b
amam <emamanik05@gmail.com>
2025-09-08 01:15:01 +0600

    correction: Update piano key sound directory and key color

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ keys.forEach((key) => {
     key.addEventListener('touchstart', (e) => {
         e.preventDefault(); // Prevents ghost clicks on mobile
         const clickedKeyId = e.target.id;
-        const pianoSound = new Audio(`./KeySounds/${clickedKeyId}.mp3`);
+        const pianoSound = new Audio(`./KeySounds2/${clickedKeyId}.mp3`);
         pianoSound.currentTime = 0; // Allow rapid retrigger
         pianoSound.play();
     });
     // Optional: Also support mouse clicks for desktop
     key.addEventListener('click', (e) => {
         const clickedKeyId = e.target.id;
-        const pianoSound = new Audio(`./KeySounds/${clickedKeyId}.mp3`);
+        const pianoSound = new Audio(`./KeySounds2/${clickedKeyId}.mp3`);
         pianoSound.currentTime = 0;
         pianoSound.play();
     });
